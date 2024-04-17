623df660f66263d9c8963a68aa993bc8c28493a9
Isacks <eddisacks@gmail.com>
2024-04-17 09:14:58 -0500

    working with models

--- database/ticket.model.js
+++ database/ticket.model.js
@@ -1,1 +1,9 @@
+import { Schema, models, model, Document } from "mongoose";
 
+const TicketSaleSchema = new Schema({
+  numbers: [{ type: Schema.Types.ObjectId, ref: "Numbers" }],
+  createdBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  createdAt: { type: Date, default: Date.now },
+  terminalId: [{ type: Schema.Types.ObjectId, ref: "Terminal" }],
+  lotteryId: [{ type: Schema.Types.ObjectId, ref: "Lottery" }],
+});
