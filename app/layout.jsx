66a0d2c2fb86e6dda2ee259f00d03263ca8b6a64
Isacks <eddisacks@gmail.com>
2024-05-07 17:20:23 -0500

    adding meta name viewport

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,6 +20,13 @@ export const metadata = {
   icons: "/assets/img/logo_loto.png",
 };
 
+export const viewport = {
+  initialScale: 1,
+  width: "device-width",
+  maximumScale: 1,
+  viewportFit: "cover",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
